13a053003b1bf6d129558d7a39a71a55d36d10cd
ngocviet <lengocviet31983@gmail.com>
2024-05-17 18:58:18 +0700

    feedback

---
+++ WebApplication2/Controllers/FeedbackController.cs
@@ -0,0 +1,66 @@
+﻿using ERP_Project.Commom;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineMobileRecharged.Common.CommonModels;
+using OnlineMobileRecharged.Controllers;
+using System.Diagnostics;
+using WebApplication2.Models;
+using System.Security.Cryptography;
+using static Mysqlx.Expect.Open.Types;
+
+namespace WebApplication2.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly MyDbContext _context;
+        private readonly ILogger<FeedbackController> _logger;
+
+        public FeedbackController(ILogger<FeedbackController> logger, MyDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["UserName"] = HttpContext.Session.GetString(StaticUser.UserName) ?? "";
+            ViewData["FullName"] = HttpContext.Session.GetString(StaticUser.FullName) ?? "";
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Feedback([FromBody] ModelFeedback model)
+        {
+            var rs = new Result() { HasError = false, Title = "" };
+
+            try
+            {
+
+                var u = new Feedback()
+                {
+                    content = model.Content,
+                    user_id = _context.Users.FirstOrDefault(x => x.is_active == true && x.username == model.UserName).ID,
+                    is_active = true,
+                    create_at = DateTime.Now,
+                    create_by = "Admin",
+                };
+
+                _context.Feedbacks.Add(u);
+                _context.SaveChanges();
+                rs.Title = "Edit success!";
+            }
+            catch
+            {
+                rs.HasError = true;
+                rs.Title = "Co loi xay ra khi EditUser!";
+            }
+
+            return Json(rs);
+        }
+    }
+    public class ModelFeedback
+    {
+        public string UserName { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ WebApplication2/wwwroot/views/myWeb/feedback/controller.js
@@ -0,0 +1,70 @@
+﻿var ctxfolder = "/views/myWeb/feedback";
+
+var app = angular.module("App_ESEIM", ["ngRoute"]);
+
+app.factory('dataservice', function ($http) {
+    $http.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+    var headers = {
+        "Content-Type": "application/json;odata=verbose",
+        "Accept": "application/json;odata=verbose"
+    };
+
+    return {
+        feedback: function (data) {
+            return $http.post('/Feedback/Feedback/', data, { headers: headers });
+        },
+    };
+});
+
+app.controller("Ctrl_ESEIM", function ($scope, $rootScope) {
+    var checkLogin = document.getElementById('userName');
+    $rootScope.username = checkLogin.value;
+});
+
+app.config(function ($routeProvider, $httpProvider, $locationProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: ctxfolder + '/index.html',
+            controller: "index"
+        })
+        .otherwise({
+            redirectTo: "/"
+        })
+        ;
+});
+
+app.controller("index", function ($scope, $rootScope,  dataservice, $http) {
+
+    $scope.model = {
+        Content: "",
+        UserName: "",
+    }
+    $scope.init = function () {
+
+    };
+
+    $scope.init();
+
+    $scope.feedback = function () {
+        if ($scope.model.Content == "") {
+            $scope.message = "Enter your feedback!";
+        } else {
+            $scope.isLoading = true;
+            $scope.model.UserName = $rootScope.username;
+            dataservice.feedback($scope.model)
+                .then(function (response) {
+                    if (!response.data.hasError) {
+                        $scope.isLoading = false;
+                        alert("Feedback Success!");
+                        $scope.message = "";
+                        $scope.model.Content = "";
+                    }
+                })
+                .catch(function (error) {
+                    console.log('')
+                    console.log("An error occurred:", error);
+                });
+        }
+    };
+});
